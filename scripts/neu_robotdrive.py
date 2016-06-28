385a46bb1634b1d8bbcf7ef50b1049cd6b358b93
zjiaxing <419082949@qq.com>
2016-06-28 17:14:03 +0800

    Update neu_robotdrive.py

--- scripts/neu_robotdrive.py
+++ scripts/neu_robotdrive.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 import roslib;roslib.load_manifest('neu_robotdrive')
 import rospyd
-from  neu_robotdrive.msg import Num, carOdom #自定义的消息
+from  neu_robotdrive.msg import Num, carOdom #Own definition of the message
 from geometry_msgs.msg import Twist
 
-import neu_pyserial as COM_ctr #自己写的串口监听模块
+import neu_pyserial as COM_ctr #Write own serial listening module
 import glob
 from math import sqrt, atan2, pow
 
@@ -35,26 +35,29 @@ class bluetooth_cmd():
         
         return Lwheelspeed, Rwheelspeed
         
-    def yawrate_to_speed(self, yawrate):#用拟合方法得到将左右轮差速转为旋转速度，两轮差速乘以系数为转速，两轮差速累计值*系数为旋转角度
-        if yawrate > 0:
-            theta_to_speed = 0.0077 #右转系数
+    def yawrate_to_speed(self, yawrate):#With the fitting method, the differential speed of the left 
+                                        # and the right wheels is converted to the rotational speed, 
+                                        #and the speed of the two wheels is multiplied by the coefficient.
+            if yawrate > 0:
+            theta_to_speed = 0.0077 #Right turn coefficient
         else:
-            theta_to_speed = 0.0076  #左转系数
+            theta_to_speed = 0.0076  #Left turn coefficient
             
-        x = (yawrate * 0.02) / theta_to_speed #采样yawrate：rad/s *0.02表示 20ms内应该转多少弧度，/0.0076是把 要转的弧度转化为左右轮速度差
+        x = (yawrate * 0.02) / theta_to_speed #Sampling yawrate：rad/s *0.02 said yawrate should turn the number of arc,
+                                              # /0.0076 is to turn the arc into the right and left wheel speed difference
         return   x
         
     def talker(self):
-        self.rec_data = COM_ctr.SerialData( datalen = 2)  #启动监听COM 线程
+        self.rec_data = COM_ctr.SerialData( datalen = 2)  #Start listening COM thread
         allport = glob.glob('/dev/ttyU*')
         port = allport[0]
         baud = 115200
         openflag = self.rec_data.open_com(port, baud)
         
-        rospy.Subscriber("/cmd_vel", Twist, self.callback)#订阅move_base发出的控制指令
+        rospy.Subscriber("/cmd_vel", Twist, self.callback)#subscribe Control command by move_base
         
         pub = rospy.Publisher('car_speed', carOdom)
-        pub_wheel = rospy.Publisher('wheel_speed', Num) #左右轮速度
+        pub_wheel = rospy.Publisher('wheel_speed', Num) #Left and right wheel speed
         
         r = rospy.Rate(500) # 100hz
         Lwheelpwm= 0
@@ -65,16 +68,17 @@ class bluetooth_cmd():
         while not rospy.is_shutdown():
             all_data = []
             if self.rec_data.com_isopen():
-                all_data = self.rec_data.next()  #接收的数据组
+                all_data = self.rec_data.next()  #Received data set
        
-            if all_data != []:  #如果没收到数据，不执行下面的
-                wheelspeed = Num()  #自己的消息
+            if all_data != []:  #If you do not receive the data, do not perform the followings
+                wheelspeed = Num()  #Own definition of the message
                 car_speed = carOdom()
                 leftspeed = all_data[0][0]
                 rightspeed = all_data[1][0]
                 wheelspeed.leftspeed = leftspeed
                 wheelspeed.rightspeed = rightspeed
-                #左右轮速度转化为机器人x轴前进速度和绕Z轴旋转的速度
+                #The speed of the left and right wheel is converted to the speed of the X axis 
+                #of the robot and the speed of the rotation of the Z axis.
                 resluts = self.speed_to_odom(leftspeed, rightspeed)
                 car_speed.x = resluts[0]
                 car_speed.y = resluts[1]
@@ -91,9 +95,9 @@ class bluetooth_cmd():
     def speed_to_odom(self, Lspeed = 0, Rspeed = 0):
         delta_speed = Rspeed - Lspeed
         if delta_speed < 0:
-            theta_to_speed = 0.0077 #右转系数
+            theta_to_speed = 0.0077 #Right turn coefficient
         else:
-            theta_to_speed = 0.0076  #左转系数
+            theta_to_speed = 0.0076  #Left turn coefficient
             
         v_th = delta_speed  * theta_to_speed / 0.02    # first : transform delta_speed to  delta_theta .   second: dived by delta_t 								(20ms), get the yawrate
         v_x = (Rspeed + Lspeed)/10.0/2.0    # Lspeed : dm/s   -- > m/s  so need to /10.0
@@ -102,7 +106,7 @@ class bluetooth_cmd():
         
     def blue_tooth_send(self, data = [], head = 'HY'):
         if data !=[] and self.rec_data.com_isopen():
-            self.rec_data.send_data(data, head)   #绕中心轴旋转 设定为0
+            self.rec_data.send_data(data, head)   #Around the central axis rotation is set to 0
 #        print data
             
 if __name__ == '__main__':
